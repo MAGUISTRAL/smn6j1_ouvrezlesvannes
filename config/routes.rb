8192ca54e542abf27644a4063b0995c7dea35ba7
Cédric RENAUD <ruthhaba@gmail.com>
2019-11-06 09:42:38 +0100

    readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'contact/index'
+  get 'team/index'
   get 'static_pages/index'
   get 'static_pages/secret'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-
+  resources :contact, only: [:index]
+  resources :team, only: [:index]
 
 end
